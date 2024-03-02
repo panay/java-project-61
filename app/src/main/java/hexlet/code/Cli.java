55dcce46c6e0865948da88188df89cc5175f1325
Сергей Панай <spanay@telematika.com>
2024-03-02 13:39:47 +0400

    create user interaction via cli
    create user interaction via cli

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,19 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+public class Cli {
+    public static void sayHello() {
+        Scanner sc = new Scanner(in);
+
+        out.println("Welcome to the Brain Games!");
+        out.println("May I have your name?");
+        String username = sc.next();
+        out.println("Hello, " + username + "!");
+
+        sc.close();
+    }
+}
