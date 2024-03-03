b6be6da044d38d2b1a2167f1a5cea716050ac0ac
Сергей Панай <spanay@telematika.com>
2024-03-03 21:05:31 +0400

    fix magic numbers

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -12,15 +12,13 @@ import static java.lang.System.out;
 
 public class App {
     public static void main(String[] args) {
-        int chosenGame = chooseGame();
-
-        if (chosenGame == 0) {
-            return;
-        }
+        Scanner scanner = Engine.getScanner();
+        int chosenGame = chooseGame(scanner);
 
         switch (chosenGame) {
             case 1:
                 Engine.sayHello();
+                break;
             case 2:
                 Even.play();
                 break;
@@ -37,12 +35,11 @@ public class App {
                 Prime.play();
                 break;
             default:
-                Engine.sayHello();
+                scanner.close();
         }
     }
 
-    private static int chooseGame() {
-        Scanner scanner = Engine.getScanner();
+    private static int chooseGame(Scanner scanner) {
         out.println("Please enter the game number and press Enter.");
         out.println("1 – Greet");
         out.println("2 – Even");
@@ -51,10 +48,11 @@ public class App {
         out.println("5 – Progression");
         out.println("6 – Prime");
         out.println("0 – Exit");
-        final int gameNum = scanner.nextInt();
-        out.println("Your choice: " + gameNum);
-
-        if (gameNum == 0) {
+        int gameNum = 0;
+        try {
+            gameNum = scanner.nextInt();
+            out.println("Your choice: " + gameNum);
+        } catch (Exception e) {
             scanner.close();
         }
 
